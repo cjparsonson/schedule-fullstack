7fb2e1191d7e97aa4cba325be34b4b0b0dd5fd0c
cjparsonson <cjparsonson@gmail.com>
2025-08-16 19:17:17 +0100

    Update - Connect request method to DB

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,1 +1,23 @@
+// frontend/scripts.js
 console.log("Everything connected fine to scripts.js");
+
+
+// Point to employee-list
+const employeeList = document.getElementById("employee-list");
+
+// Test fetch
+fetch("http://localhost:3000/api/employees")
+    .then(response => {
+        console.log("The node application responded");
+        return response.json();
+    }).then(data => {
+        console.log("Employee data received");
+        console.log(data);
+        // Clear the employee list
+        employeeList.innerHTML = "";
+        // Populate the employee list
+        employeeData = data[0];
+        const li = document.createElement("li");
+        li.textContent = `${employeeData.employee_id} - ${employeeData.name}`;
+        employeeList.appendChild(li);
+    });
--- index.js
+++ index.js
@@ -3,23 +3,29 @@ import express from "express";
 const app = express();
 const port = 3000;
 const { Pool } = pg;
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "postgres",
+  password: "postgres",
+  port: 5432,
+  idleTimeoutMillis: 300,
+});
+
+app.use((req, res, next) => {
+  // Middleware to set CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
+  next(); // Proceed to the next middleware or route handler
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World from Express!");
 });
 
 app.get("/api/employees", async (req, res) => {
-  const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "postgres",
-    password: "postgres",
-    port: 5432,
-    idleTimeoutMillis: 300,
-  });
-  const result = await pool.query("SELECT * FROM employee");
-  res.json(result.rows);
-  await pool.end();
+  const resultDb = await pool.query("SELECT * FROM employee");
+  const resultArray = resultDb.rows;
+  res.json(resultArray);
 });
 
 app.listen(port, () => {
